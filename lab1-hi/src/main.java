6e77180842fe4748e41dd62a26b4c0540b85d893
mmktm <kettamas.8065@gmail.com>
2019-06-26 18:21:29 +0700

    1821

---
+++ lab1-hi/src/main.java
@@ -0,0 +1,29 @@
+
+public class main {
+ public static void main(String args[])
+ {
+	 System.out.println("hello world");
+	 
+//	 int a = 10;
+//		double price = 10.5;
+//		String name = "eye";
+//		
+//		for(int i=0 ; i<5 ; i++)
+//			System .out.println(name);
+//		
+//		int[] num = {1,2,3,4,9,10};
+//		for(int i=0 ; i<num.length ; i++)
+//			System.out.println(num[i]);
+//		
+//		int[] num2 = new int[100];
+//		
+//		String[] fruit = {"banana","apple"};
+//		for(String f : fruit)
+//			System.out.println(f);
+		
+		String tel = "085a039a616";
+		String[] ts = tel.split("a");
+		for(String t : ts )
+			System.out.println(t);
+ }
+}
